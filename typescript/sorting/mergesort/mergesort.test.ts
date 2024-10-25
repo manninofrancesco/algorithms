a011a03a78db90661555a26647d53f23c5f2e1b9
francescom <francescom@monade.io>
2024-10-25 19:10:20 +0200

    feat(tests): introduce unit test & complete mergesort

--- typescript/sorting/mergesort/mergesort.test.ts
+++ typescript/sorting/mergesort/mergesort.test.ts
@@ -1,13 +1,15 @@
 import { mergeSort } from "./mergesort";
 
+console.clear();
+
 const input: number[] = [5, 3, 8, 2, 1, 4];
 const expected: number[] = [1, 2, 3, 4, 5, 8];
 const result = mergeSort(input);
-const message: string = `Expected: ${expected}\nGot: ${result}`;
 
 if(JSON.stringify(expected) ===  JSON.stringify(result)){
     console.log("🟢 Test passed!");
 }else{
-    console.log(message);
+    const message: string = `Input: ${input}\nExpected: ${expected}\nGot: ${result}`;
+    console.error(`\n\n----------------------------\n\n${message}`);
     console.log("🔴 Test failed!");
 }
--- typescript/sorting/mergesort/mergesort.ts
+++ typescript/sorting/mergesort/mergesort.ts
@@ -1,5 +1,35 @@
-export const mergeSort = (arr: number[]): number[] => {
-    let sortedArray: number[] = [];
+export const mergeSort = (input: number[]): number[] => {
+  if (input.length <= 1) {
+    return input;
+  }
+  
+  const middleIndex: number = Math.round(input.length / 2);
+  const leftSubArray: number[] = mergeSort(input.slice(0, middleIndex));
+  const rightSubArray: number[] = mergeSort(
+    input.slice(middleIndex, input.length)
+  );
 
-    return sortedArray;
-}
+  return merge(leftSubArray, rightSubArray);
+};
+
+const merge = (left: number[], right: number[]): number[] => {
+  const result: number[] = [];
+  let leftIndex: number = 0;
+  let rightIndex: number = 0;
+
+  while (leftIndex < left.length || rightIndex < right.length) {
+    if (leftIndex < left.length && rightIndex < right.length) {
+      if (left[leftIndex] <= right[rightIndex]) {
+        result.push(left[leftIndex++]);
+      } else {
+        result.push(right[rightIndex++]);
+      }
+    } else if (leftIndex < left.length) {
+      result.push(left[leftIndex++]);
+    } else if (rightIndex < right.length) {
+      result.push(right[rightIndex++]);
+    }
+  }
+
+  return result;
+};
